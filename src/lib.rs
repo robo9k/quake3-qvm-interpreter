16770589bd4109fb0e744ef41e3c5e781e735d2d
robo9k <robo9k@symlink.io>
2020-05-21 14:41:30 +0200

    Add first implementation of loading QVM into `Interpreter`

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use quake3_qvm::bytecode::Address;
+use quake3_qvm::{QVM, Instruction, bytecode::Address};
 use std::collections::BTreeMap;
 
 // Both Symbol and SymbolMap probably belong in quake3-qvm itself
@@ -81,6 +81,52 @@ impl SymbolMap {
     }
 }
 
+pub const STACK_SIZE: usize = 0x10000;
+
+#[derive(Debug)]
+pub struct Interpreter {
+    instructions: Vec<Instruction>,
+    program_counter: usize,
+    stack_pointer: usize,
+    data: Vec<u8>,
+}
+
+impl Interpreter {
+    #[must_use]
+    pub fn new(qvm: QVM) -> Self {
+        let qvm_data = qvm.data();
+        let lit = qvm.lit();
+        let len: usize = qvm_data.len() * 4 + lit.len() + qvm.bss_length() as usize * 4;
+        println!("len {}", len);
+        let mut data: Vec<u8> = Vec::with_capacity(len);
+        for word in qvm_data {
+            data.push(((word & 0x000000FF) >>  0) as u8);
+            data.push(((word & 0x0000FF00) >>  8) as u8);
+            data.push(((word & 0x00FF0000) >> 16) as u8);
+            data.push(((word & 0xFF000000) >> 24) as u8);
+        }
+        for byte in lit {
+            data.push(*byte);
+        }
+        println!("data {:?}", data);
+
+        Self {
+            instructions: qvm.instructions().iter().map(|i| i.clone()).collect(), // FIXME: QVM API
+            program_counter: 0,
+            stack_pointer: len,
+            data: data,
+        }
+    }
+
+    fn execute_instruction(&mut self, instruction: &Instruction) -> Result<(), ()> {
+        todo!();
+    }
+
+    fn execute(&mut self) -> Result<(), ()> {
+        todo!();
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -101,5 +147,19 @@ mod tests {
     fn symbol_map_name_exact() {
         let map = SymbolMap::with_symbols(vec![Symbol::new(42, "name")]);
         assert_eq!(map.symbol_name(42), Some("name".into()));
+    }
+
+    #[test]
+    fn interpreter_new() {
+        let qvm = QVM::new(vec![Instruction::UNDEF], vec![0xDEADBEEF], vec![], 0).unwrap();
+        let interpreter = Interpreter::new(qvm);
+        eprintln!("interpreter {:?}", interpreter);
+        assert_eq!(interpreter.program_counter, 0);
+        assert_eq!(interpreter.stack_pointer, 4);
+        assert_eq!(interpreter.data[0], 0xEF);
+        assert_eq!(interpreter.data[1], 0xBE);
+        assert_eq!(interpreter.data[2], 0xAD);
+        assert_eq!(interpreter.data[3], 0xDE);
+        assert_eq!(interpreter.instructions, vec![Instruction::UNDEF]);
     }
 }
